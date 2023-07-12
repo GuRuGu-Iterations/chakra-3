a06e22453d9a8d782e8fa561b2642751f079ef66
GuRuGu <peter.k.nets@gmail.com>
2023-07-12 10:13:49 +0300

    feat: set up Login and Registration forms

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -1,5 +1,59 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
 function Login() {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.email}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type='email'
+          {...register('email', { required: 'Email is required' })}
+        />
+        {!errors.email ? (
+          <FormHelperText>Enter the email you registered with.</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.email}>
+        <FormLabel>Password</FormLabel>
+        <Input
+          type='password'
+          {...register('password', { required: 'Please enter your password' })}
+        />
+        {!errors.password ? (
+          <FormHelperText>Enter your password.</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Button colorScheme='whiteAlpha' type='submit' marginBlockStart='1rem'>
+        Log In
+      </Button>
+    </form>
+  );
 }
 
 export { Login };
--- src/pages/auth/registration/registration.tsx
+++ src/pages/auth/registration/registration.tsx
@@ -1,5 +1,79 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 function Registration() {
-  return <div>Registration</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const watchPassword = watch('password');
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.email}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type='email'
+          {...register('email', { required: 'Email is required' })}
+        />
+        {!errors.email ? (
+          <FormHelperText>Enter the email you registered with.</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.email}>
+        <FormLabel>Password</FormLabel>
+        <Input
+          type='password'
+          {...register('password', { required: 'Please enter your password' })}
+        />
+        {!errors.password ? (
+          <FormHelperText>Enter your password.</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.email}>
+        <FormLabel>Confirm password</FormLabel>
+        <Input
+          type='password'
+          {...register('confirmPassword', {
+            required: 'Please enter your password',
+            validate: (value) =>
+              value === watchPassword || 'Passwords do not match',
+          })}
+        />
+        {!errors.confirmPassword ? (
+          <FormHelperText>Confirm your password.</FormHelperText>
+        ) : (
+          <FormErrorMessage>{errors.confirmPassword.message}</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Button colorScheme='whiteAlpha' type='submit' marginBlockStart='1rem'>
+        Register
+      </Button>
+    </form>
+  );
 }
 
 export { Registration };
